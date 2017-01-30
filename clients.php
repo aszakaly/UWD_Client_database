66d166c90f186d79ddc260255b11fcfab8882245
Akos Szakaly <akos.szakaly@gmail.com>
2017-01-30 17:11:24 +0100

    Query clients list

--- clients.php
+++ clients.php
@@ -2,52 +2,67 @@
 session_start();
 
 //if user is not logged in
-if (!$_SESSION['loggedInUser']) {
+if(!$_SESSION['loggedInUser']) {
 
     //send the user to login page
     header("Location: index.php");
 }
 
+//connect to database
+include('includes/connection.php');
 
+//query & result
+$query = "SELECT * FROM clients";
+$result = mysqli_query( $conn, $query );
+
+// close connection
+mysqli_close($conn);
 
 include('includes/header.php');
 ?>
 
+<h1>Client Address Book</h1>
+<table class="table table-striped table-bordered">
+   <tr>
+       <th>Name</th>
+       <th>Email</th>
+       <th>Phone</th>
+       <th>Address</th>
+       <th>Company</th>
+       <th>Notes</th>
+       <th>Edit</th>
+   </tr>
 
-        <h1>Client Address Book</h1>
-        <table class="table table-striped table-bordered">
-           <tr>
-               <th>Name</th>
-               <th>Email</th>
-               <th>Phone</th>
-               <th>Address</th>
-               <th>Company</th>
-               <th>Notes</th>
-               <th>Edit</th>
-           </tr>
-           <tr>
-               <td>John Doe</td>
-               <td>johndoe@email.com</td>
-               <td>(123) 456-7890</td>
-               <td>111 Address Street, Calgary, AB T1G 2KY</td>
-               <td>Brightside Studios Inc.</td>
-               <td>Usually pays early. He's awesome.</td>
-               <td><a href="edit.php" type="button" class="btn btn-default btn-primary btn-sm"><span class="glyphicon glyphicon-edit"></span></a></td>
-           </tr>
-           <tr>
-               <td>Jane Doe</td>
-               <td>janedoe@email.com</td>
-               <td>(123) 456-7890</td>
-               <td>123a Address Street, Calgary, AB T1G 2KY</td>
-               <td>Brightside Studios Inc.</td>
-               <td>Nice lady. Pays in high fives though...</td>
-               <td><a href="edit.php" type="button" class="btn btn-default btn-primary btn-sm"><span class="glyphicon glyphicon-edit"></span></a></td>
-           </tr>
-           <tr>
-               <td colspan="7"><div class="text-center"><a href="add.php" type="button" class="btn btn-sm btn-success"><span class="glyphicon glyphicon-plus"></span> Add Client</a></div>
-           </tr>
+   <?php
 
-        </table>
+    if( mysqli_num_rows($result) > 0 ) {
+
+        //we have data
+        //output the data
+
+        while( $row = mysqli_fetch_assoc($result) ) {
+            echo "<tr>";
+
+            echo "<td>" . $row['name'] . "</td><td>" . $row['email'] . "</td><td>" $row['phone']."</td><td>" . $row['address'] . "</td><td>" . $row['company'] . "</td><td>" . $row['notes'] . "</td>";
+
+            echo '<td><a href="edit.php?id=' . $row['id'] . '" type="button" class="btn btn-primary btn-sm">
+                    <span class="glyphicon glyphicon-edit"></span>
+                    </a></td>';
+
+            echo "</tr>";
+        }
+    } else { //if no clients
+        echo "<div class='alert alert-warning'>You have no clients!</div>"
+    }
+
+    mysqli_close($conn);
+
+    ?>
+    <tr>
+        <td colspan="7"><div class="text-center"><a href="add.php" type="button" class="btn btn-sm btn-success"><span class="glyphicon glyphicon-plus"></span> Add Client</a></div></td>
+    </tr>
+
+</table>
 <?php
 include('includes/footer.php');
 ?>
